0cb9422b76c8b71c7a22e84f12546ca96dfe09af
Wendel Mendes Rodrigues <wendel.rodrigues@etec.sp.gov.br>
2024-03-05 17:53:40 -0300

    Controllers

---
+++ myFirstApi/app/Http/Controllers/CarrinhoController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class CarrinhoController extends Controller
+{
+    public function Adicioanar()  
+    {
+
+        return 0;
+    }
+
+    public function Remover()  
+    {
+        
+        return 0;
+    }
+
+    public function Editar() 
+    {
+        
+        return 0;
+    }
+}
---
+++ myFirstApi/app/Http/Controllers/ContaController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class ContaController extends Controller
+{
+    public function EditarPerfil()
+    {
+        
+        return 0;
+    }
+}
